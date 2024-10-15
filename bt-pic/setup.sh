e81ab93c5cf28daf0c22024bd14d815165fd4c3d
btpaulie <33455765+btpaulie@users.noreply.github.com>
2024-10-14 23:58:55 -0400

    Update setup.sh

--- bt-pic/setup.sh
+++ bt-pic/setup.sh
@@ -2,7 +2,7 @@
 
 mkdir /srv/photoprism
 cd /srv/photoprism
-wget -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/docker-compose.yml
-wget -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/.env
+curl -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/docker-compose.yml
+curl -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/.env
 nano .env
 docker-compose up -d
