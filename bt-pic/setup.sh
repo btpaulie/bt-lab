044b6d2257ae2f8437377da33e214f241feea596
btpaulie <33455765+btpaulie@users.noreply.github.com>
2024-10-15 00:10:56 -0400

    Update setup.sh

--- bt-pic/setup.sh
+++ bt-pic/setup.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-mkdir /srv/photoprism
-cd /srv/photoprism
+# Script intended to be run as root.
+
+# Create directory
+mkdir /srv/photoprism && cd /srv/photoprism
+
+# Curl compose.yml & .env
 curl -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/docker-compose.yml
 curl -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/.env
 nano .env
+
+# Create mount directory
+mkdir /mnt/photo
+
+# Edit fstab
+
+read -p "Photo server ipv4 address:" IPV4
+read -p "Photo server photo share:" PSHARE
+
+cat eof
+
+# Start containers
 docker-compose up -d
