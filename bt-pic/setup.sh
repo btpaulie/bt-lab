3dc20c5822e25321ac461a1f061c8643f9c5e5b3
btpaulie <33455765+btpaulie@users.noreply.github.com>
2024-10-15 16:35:40 -0400

    functions more

--- bt-pic/setup.sh
+++ bt-pic/setup.sh
@@ -3,13 +3,14 @@
 # Script intended to be run as root.
 
 # Functions
-
 DEPEND () {
     apt install curl -y && apt install cifs-utils -y
 }
 
 DIRCREATE () {
     mkdir /srv/photoprism && cd /Srv/photoprism
+    mkdir /mnt/$PSHARE
+    mount -a
 }
 
 PULL_FILES () {
@@ -17,36 +18,31 @@ PULL_FILES () {
     curl -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/.env
 }
 
-SMB_MOUNT () {
+SMB_SETUP () {
     read -p "Photo server ipv4 address:" IPV4
     read -p "Photo server photo share:" PSHARE
     read -p "Photo server username:" SMBUSR
     read -p "Photo server password:" SMBPW
     cat <<EOF > /etc/fstab
-    //$IPV4/$PSHARE /mnt/$PSHARE cifs credentials=/etc/.cred,uid=1000,gid=1000 0 0 
-    EOF
+    //$IPV4/$PSHARE /mnt/$PSHARE cifs credentials=/etc/.cred,uid=1000,gid=1000 0 0
+EOF
+    cat <<EOF > /etc/.cred
+    username=$SMBUSR
+    password=$SMBPW
+EOF
+}
+
+CONTAINER_RUN () {
+    cd /srv/photoprism
+    docker-compose up -d
+    docker stats
 }
 
 # Run 
 DEPEND #install dependencies
-DIRCREATE #create photoprism dir, cd to it
 PULL_FILES #curl compose & .env
+SMB_SETUP #get server IP, share name, username, password; mount drive in /mnt
+DIRCREATE #create photoprism dir, cd to it, create mount, mount
+CONTAINER_RUN #run container, load `docker stats`
 
-
-# Set up drive mounts
-
-# Edit fstab
-
-
-# Create credential file
-cat <<EOF > /etc/.cred
-username=$SMBUSR
-password=$SMBPW
-EOF
-
-mkdir /mnt/$PSHARE #create mount for photo share
-mount -a #mount drives
-
-# Start containers
-docker-compose up -d
 
