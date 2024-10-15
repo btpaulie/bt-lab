6bbf7435e7611181dc7788bd05a801b09a793003
btpaulie <33455765+btpaulie@users.noreply.github.com>
2024-10-14 23:13:50 -0400

    Update pull.sh

--- bt-pic/pull.sh
+++ bt-pic/pull.sh
@@ -2,8 +2,7 @@
 
 mkdir /srv/photoprism
 cd /srv/photoprism
-wget https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/docker-compose.yml
-wget https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/photoprism.env
-echo "Remember to fill in your .env file variables before running the compose."
-nano photoprism.env
+wget -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/docker-compose.yml
+wget -O https://raw.githubusercontent.com/btpaulie/bt-lab/refs/heads/main/bt-pic/photoprism.env
+nano .env
 docker-compose up -d
